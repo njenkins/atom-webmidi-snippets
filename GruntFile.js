a7cb857208c3178f9943c1b2e6b5ed7c4d72c0fa
njenkins <web@nathanjenkins.com.au>
2016-04-11 17:54:32 +1000

    Test for duplicate keys

--- GruntFile.js
+++ GruntFile.js
@@ -9,8 +9,28 @@ module.exports = function(grunt) {
       }
     }
   });
-
   grunt.loadNpmTasks('grunt-lintspaces');
-
-  grunt.registerTask('default', ['lintspaces']);
+  //Ensure that all snippet shortcuts are unique.
+    grunt.registerTask('prefixconflict', function(){
+      var snippetFiles = grunt.file.expand('snippets/**/*');
+      var prefixKeys = [];
+      snippetFiles.forEach(function(snippetFilePath){
+        //If path is to a snippet config file get file content
+        if(snippetFilePath.indexOf('.cson') !== -1){
+          var snippetFileContent = grunt.file.read(snippetFilePath);
+          var asArray = snippetFileContent.split('\n');
+          if(typeof asArray[2] !== 'undefined'){
+            var prefixKey = asArray[2].split(':')[1].trim();
+            if(prefixKeys.indexOf(prefixKey) == -1){
+              prefixKeys.push(prefixKey);
+            }
+            else {
+              grunt.fail.warn(prefixKey + ' already exists');
+            }
+          }
+        }
+      });
+      grunt.log.ok('No duplicate prefix keys found in ' + prefixKeys.length + ' files.');
+    });
+  grunt.registerTask('default', ['prefixconflict','lintspaces']);
 };
